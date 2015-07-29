02166e53bac593fb32805d004330ad12787be344
Gopal V <gopalv@apache.org>
2015-07-29 10:28:42 -0700

    fix white space

--- src/main/java/org/notmysock/hive/hooks/AndOrRewriteHook.java
+++ src/main/java/org/notmysock/hive/hooks/AndOrRewriteHook.java
@@ -57,7 +57,7 @@ import com.google.common.collect.Lists;
 public class AndOrRewriteHook extends AbstractSemanticAnalyzerHook {
 	protected static final Log LOG = LogFactory.getLog(AndOrRewriteHook.class
 			.getName());
-	
+
 	@Override
 	public void postAnalyze(HiveSemanticAnalyzerHookContext context,
 			List<Task<? extends Serializable>> rootTasks)
@@ -71,15 +71,16 @@ public class AndOrRewriteHook extends AbstractSemanticAnalyzerHook {
 		}
 		for (MapWork w : work) {
 			Set<Operator<?>> ops = w.getAllRootOperators();
-			for(Operator p : ops) {
-				//Optimization criteria - TS followed by FIL 
+			for (Operator p : ops) {
+				// Optimization criteria - TS followed by FIL
 				if (p.getType() == OperatorType.TABLESCAN) {
 					List<Operator> children = p.getChildOperators();
 					LOG.info("GOPAL : " + children);
 					if (children != null && children.size() == 1) {
 						Operator first = children.get(0);
 						if (first.getType() == OperatorType.FILTER) {
-							processTableFilter((TableScanOperator)p, (FilterOperator)first);
+							processTableFilter((TableScanOperator) p,
+									(FilterOperator) first);
 						}
 					} else {
 						break;
@@ -116,14 +117,15 @@ public class AndOrRewriteHook extends AbstractSemanticAnalyzerHook {
 		}
 		GenericUDF first = null;
 		for (ExprNodeDesc inner : any) {
-			if(inner instanceof ExprNodeGenericFuncDesc) {
+			if (inner instanceof ExprNodeGenericFuncDesc) {
 				final ExprNodeGenericFuncDesc genFunc = (ExprNodeGenericFuncDesc) inner;
 				final GenericUDF udf = genFunc.getGenericUDF();
 				if (first == null) {
 					first = udf;
 				} else {
 					if (!first.getClass().equals(udf.getClass())) {
-						LOG.info("The inner ANY clauses are not uniform : " + first + " ~~> " + udf);
+						LOG.info("The inner ANY clauses are not uniform : "
+								+ first + " ~~> " + udf);
 						return;
 					}
 				}
@@ -132,17 +134,17 @@ public class AndOrRewriteHook extends AbstractSemanticAnalyzerHook {
 				return;
 			}
 		}
-		
+
 		// all parts of ANY are the same UDF type as first
 		LOG.info("We have " + any.size() + " instances of " + first);
-		
+
 		if (FunctionRegistry.isStateful(first) == false
 				&& FunctionRegistry.isDeterministic(first) == false) {
 			LOG.info(" Turns out " + first + " is non-deterministic. Exiting");
 		}
-		
+
 		final List<ExprNodeDesc> subexpr;
-		
+
 		if (first instanceof GenericUDFOPAnd) {
 			subexpr = decomposeAndPredicates(any);
 		} else {
@@ -155,7 +157,7 @@ public class AndOrRewriteHook extends AbstractSemanticAnalyzerHook {
 
 	private ExprNodeDesc balanceAnyPredicates(List<ExprNodeDesc> any) {
 		// turn ANY expr node desc list into a balanced tree
-		
+
 		Queue<ExprNodeDesc> queue = new LinkedList<ExprNodeDesc>();
 		queue.addAll(any);
 		while (!queue.isEmpty()) {
@@ -176,10 +178,10 @@ public class AndOrRewriteHook extends AbstractSemanticAnalyzerHook {
 		Preconditions.checkArgument(false, "This is a trap!");
 		return null;
 	}
-	
+
 	private ExprNodeDesc balanceAllPredicates(List<ExprNodeDesc> all) {
 		// turn ALL expr node desc list into a balanced tree
-		
+
 		Queue<ExprNodeDesc> queue = new LinkedList<ExprNodeDesc>();
 		queue.addAll(all);
 		while (!queue.isEmpty()) {
@@ -205,39 +207,42 @@ public class AndOrRewriteHook extends AbstractSemanticAnalyzerHook {
 		final ExprNodeColumnDesc column;
 		final Set<ExprNodeDescEqualityWrapper> constants = new HashSet<ExprNodeDescEqualityWrapper>();
 		int items = 0;
-		
+
 		public AndRange(ExprNodeColumnDesc col) {
 			this.column = col;
 		}
-		
+
 		public void add(ExprNodeConstantDesc cons) {
 			items++;
-			LOG.info(column + " += " + cons.getExprString() + String.format(" [%d items, ndv %d]", items, cost()));
+			LOG.info(column + " += " + cons.getExprString()
+					+ String.format(" [%d items, ndv %d]", items, cost()));
 			constants.add(new ExprNodeDescEqualityWrapper(cons));
 		}
-		
+
 		public ExprNodeDesc getExpr() {
 			GenericUDFIn in = new GenericUDFIn();
-			List<ExprNodeDesc> expressions = new ArrayList<ExprNodeDesc>(constants.size() + 1);
+			List<ExprNodeDesc> expressions = new ArrayList<ExprNodeDesc>(
+					constants.size() + 1);
 			// IN(col, ... constants)
 			expressions.add(column);
 			for (ExprNodeDescEqualityWrapper wrap : constants) {
-				expressions.add(wrap.getExprNodeDesc());	
+				expressions.add(wrap.getExprNodeDesc());
 			}
-			return new ExprNodeGenericFuncDesc(
-					TypeInfoFactory.booleanTypeInfo, in, expressions);
+			return new ExprNodeGenericFuncDesc(TypeInfoFactory.booleanTypeInfo,
+					in, expressions);
 		}
-		
+
 		public int cost() {
 			return constants.size();
 		}
 	}
-	
+
 	private List<ExprNodeDesc> decomposeAndPredicates(List<ExprNodeDesc> any) {
 		DefaultHashMap<ExprNodeDescEqualityWrapper, AndRange> expr = new DefaultHashMap<ExprNodeDescEqualityWrapper, AndRange>() {
 			@Override
 			public AndRange defaultValue(ExprNodeDescEqualityWrapper wrap) {
-				ExprNodeColumnDesc key = (ExprNodeColumnDesc) wrap.getExprNodeDesc();
+				ExprNodeColumnDesc key = (ExprNodeColumnDesc) wrap
+						.getExprNodeDesc();
 				if (key.getTypeInfo().getCategory() == Category.PRIMITIVE) {
 					PrimitiveTypeInfo pti = (PrimitiveTypeInfo) key
 							.getTypeInfo();
@@ -246,53 +251,62 @@ public class AndOrRewriteHook extends AbstractSemanticAnalyzerHook {
 				return null;
 			}
 		};
-		
-		// generate a sub expression from a list of AND trees into ALL expressions
-  		for (ExprNodeDesc and : any) {
-  			// this is recursive, but we're ok with that
-  			List<ExprNodeDesc> all = ExprNodeDescUtils.split(and);
-  			boolean oneMatch = false; // at least one AND has to return a col = const to consider the optimization
-  			for (ExprNodeDesc inner : all) {
-  				final ExprNodeGenericFuncDesc genFunc = (ExprNodeGenericFuncDesc) inner;
+
+		// generate a sub expression from a list of AND trees into ALL
+		// expressions
+		for (ExprNodeDesc and : any) {
+			// this is recursive, but we're ok with that
+			List<ExprNodeDesc> all = ExprNodeDescUtils.split(and);
+			boolean oneMatch = false; // at least one AND has to return a col =
+										// const to consider the optimization
+			for (ExprNodeDesc inner : all) {
+				final ExprNodeGenericFuncDesc genFunc = (ExprNodeGenericFuncDesc) inner;
 				final GenericUDF udf = genFunc.getGenericUDF();
-  				if (udf instanceof GenericUDFOPEqual) {
-  					List<ExprNodeDesc> eq = genFunc.getChildren();
-  					ExprNodeColumnDesc col = null;
-  					ExprNodeConstantDesc cons = null;
-  					for (ExprNodeDesc hs : eq) {
-  						if (hs instanceof ExprNodeColumnDesc) {
-  							col = (ExprNodeColumnDesc) hs;
-  						} else if (hs instanceof ExprNodeConstantDesc) {
-  							cons = (ExprNodeConstantDesc) hs;
-  						} else {
-  							LOG.info("Unexpected type in AND expr " + hs);
-  						}
-  					}
-  					if (cons != null || col != null) {
-  						AndRange range = expr.getDefault(new ExprNodeDescEqualityWrapper(col));
-  						if (range != null) {
-  							// we can't help unknown range types
-  	  						oneMatch = true;
-  	  						range.add(cons);
-  						}
-  					}
-  				}
-  			}
-  			if (oneMatch != true) {
-  				LOG.info("We can't fold the AND, due to " + and.getExprString());
-  				return null;
-  			}
+				if (udf instanceof GenericUDFOPEqual) {
+					List<ExprNodeDesc> eq = genFunc.getChildren();
+					ExprNodeColumnDesc col = null;
+					ExprNodeConstantDesc cons = null;
+					for (ExprNodeDesc hs : eq) {
+						if (hs instanceof ExprNodeColumnDesc) {
+							col = (ExprNodeColumnDesc) hs;
+						} else if (hs instanceof ExprNodeConstantDesc) {
+							cons = (ExprNodeConstantDesc) hs;
+						} else {
+							LOG.info("Unexpected type in AND expr " + hs);
+						}
+					}
+					if (cons != null || col != null) {
+						AndRange range = expr
+								.getDefault(new ExprNodeDescEqualityWrapper(col));
+						if (range != null) {
+							// we can't help unknown range types
+							oneMatch = true;
+							range.add(cons);
+						}
+					}
+				}
+			}
+			if (oneMatch != true) {
+				LOG.info("We can't fold the AND, due to " + and.getExprString());
+				return null;
+			}
 		}
-  		
-  		ArrayList<AndRange> subExprs = new ArrayList<AndRange>();
+
+		ArrayList<AndRange> subExprs = new ArrayList<AndRange>();
 		for (AndRange subExpr : expr.values()) {
 			if (subExpr.cost() < 1024) {
 				subExprs.add(subExpr);
 			}
-			LOG.info("GOPAL: " + subExpr.column + " has a cost of " + subExpr.cost() + " and " + subExpr.items);
+			LOG.info("GOPAL: " + subExpr.column + " has a cost of "
+					+ subExpr.cost() + " and " + subExpr.items);
 		}
-		
-  		Collections.sort(subExprs, new Comparator<AndRange>() {
+
+		if (subExprs.isEmpty()) {
+			LOG.info("GOPAL: all expressions are too expensive to pull out");
+			return null;
+		}
+
+		Collections.sort(subExprs, new Comparator<AndRange>() {
 			@Override
 			public int compare(AndRange o1, AndRange o2) {
 				if (o1.cost() > o2.cost()) {
@@ -302,8 +316,13 @@ public class AndOrRewriteHook extends AbstractSemanticAnalyzerHook {
 				}
 				return 0;
 			}
-  		});
-  		
+		});
+
+		for (AndRange subExpr : subExprs) {
+			LOG.info("Partial expression for pull up "
+					+ subExpr.getExpr().getExprString());
+		}
+
 		return null;
 	}
 }
